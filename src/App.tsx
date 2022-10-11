29efc608b002e7c0728686718492daf883630675
Ahmed Albarghouti <31451399+AhmedAlbarghouti@users.noreply.github.com>
2022-10-11 13:36:50 -0400

    complete project-left

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import Project from './Components/Project/Project';
+import githubIcon from './assets/icons/github.svg'
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Hero></Hero>
-      <h2 className='section-header' >Projects</h2>
-      <Project  projectTitle="nice" projectDesc="descc" projectTags={[12,245,123]}></Project>
+      <div className="projects-header"><h2 className='section-header' >Projects</h2>
+      <a className='github-link' href="https://github.com/AhmedAlbarghouti">Github <img src={githubIcon} alt="github" /></a></div>
+      
+      
+      <Project  isFeaturedProject={true} projectTitle="nice" projectDesc="Black is a very powerful color... Well kind of. Black is the absence of color (ie light), which means it is not really a color itself. It has very high contrast, especially with white, so it's a popular color to use on the web." projectTags={["HTML","CSS","123",'lol','test','Node','React']}></Project>
+      <Project   projectTitle="nice" projectDesc="descc" projectTags={["HTML","CSS","123"]}></Project>
     </div>
   );
 }
--- src/Components/Project/Project.tsx
+++ src/Components/Project/Project.tsx
@@ -1,10 +1,11 @@
 import './Project.css'
-
+import githubIcon from '../../assets/icons/github.svg';
+import playIcon from '../../assets/icons/play.svg';
 type ProjectProps = {
     isFeaturedProject?: boolean,
     projectTitle: String,
     projectDesc: String,
-    projectTags: number[]
+    projectTags: String[]
 }
 
 export default function Project(props:ProjectProps){
@@ -14,7 +15,27 @@ export default function Project(props:ProjectProps){
 
     return(
         <div className="Project">
-            
+             <div className="project-info">
+            {props.isFeaturedProject ? <span className='project-featured'>FEATURED PROJECT</span> : ''}
+
+                <div className="project-header">
+                <h4 className='project-title'>Project Name</h4>
+                <div className="project-links">
+                    <a href=""><img src={githubIcon} alt="github" /></a>
+                    <a href=""><img src={playIcon} alt="play" /></a>
+                </div>
+                </div>
+                
+                <p className="project-desc">{props.projectDesc}</p>
+                <h5 className='project-tags-title'>Technologies Used</h5>
+                <div className="project-tags">
+                    {props.projectTags.map(tag =>(
+                        <div className="project-tag">{tag}</div>
+                    ))}
+                </div>
+                
+            </div>
+            <img src="" alt="Project" className="project-img" />
             
         </div>
     );
