532ac9ed7c507c28f32d6813582678d20080e8fb
Ahmed Albarghouti <31451399+AhmedAlbarghouti@users.noreply.github.com>
2022-10-17 23:23:48 -0400

    add mobile navigation

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import './Navbar.css'
+import { useEffect, useState } from 'react';
+import './Navbar.css';
 import {Link} from 'react-router-dom';
-import pdf from '../../assets/resume.pdf'
+import pdf from '../../assets/resume.pdf';
+import menuImg from '../../assets/icons/menu.svg';
+import xImg from '../../assets/icons/x.svg';
 
 export default function Navbar(){
-    return(
-        <nav>
-            <Link to='/' >Ahmed Albarghouti</Link>
-            
+    const [showMenu, setShowMenu] = useState(false);
 
-            <div className="routes">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-                <button className='main-btn'><a href={pdf} rel="noreferrer" target="_blank" >Resume</a></button>
+    const handleLinkClick = () =>{
+        if(showMenu){
+            setShowMenu(false);
+        }
+    }
+    
+    
+    return(
+        <nav >
+
+            <Link to='/' >Ahmed Albarghouti</Link> 
+            <img className='menu-icon' src={showMenu ? xImg : menuImg } alt="menu" onClick={() => setShowMenu(!showMenu)} ></img>
+            <div className={showMenu ? 'navigation-menu expanded' : "navigation-menu"}>
+
+                <ul>
+                    <li><Link onClick={handleLinkClick} to="/">Home</Link></li>
+                    <li><Link onClick={handleLinkClick} to="/about">About</Link></li>
+                    <li> <Link onClick={handleLinkClick} to="/contact">Contact</Link></li>
+                    <li><button className='main-btn'><a href={pdf} rel="noreferrer" target="_blank" >Resume</a></button></li>
+                </ul>
+                
+                
+               
+                
                 
             </div>
+
+            
+            
+            
+            
+        
         </nav>
     )
 }
